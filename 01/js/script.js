5b959c8997afb3bf7055f9efebe0a3c97dc8c999
Valera <bgvalera@gmail.com>
2022-05-08 20:29:50 +0300

    первый урок

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,1 +1,15 @@
-console.log(777);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+let film = prompt('Один из последних просмотренных фильмов?', '');
+let grade = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[film] = grade;
+film = prompt('Один из последних просмотренных фильмов?', '');
+grade = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[film] = grade;
+console.log(personalMovieDB);
