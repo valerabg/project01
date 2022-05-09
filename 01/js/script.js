02870011b800c91ff6397c9b1a05e3f5817190a6
Valera <bgvalera@gmail.com>
2022-05-09 13:33:38 +0300

    Условия и циклы

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,15 +1,33 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-let personalMovieDB = {
-    count: 0,
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
-let film = prompt('Один из последних просмотренных фильмов?', '');
-let grade = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[film] = grade;
-film = prompt('Один из последних просмотренных фильмов?', '');
-grade = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[film] = grade;
+
+let i = 0;
+ while (i < 2) {
+    i++;
+    let film = prompt('Один из последних просмотренных фильмов?', '');
+    if (!film)
+        i = 0;
+    else if (film.length > 50)
+        i = 0;
+    else {
+        let grade = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[film] = grade;
+    }
+}
+
+if (personalMovieDB.count < 10 && personalMovieDB.count != 0)
+    alert('Просмотрено довольно мало фильмов');
+else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30)
+    alert('Вы классический зритель');
+else if (personalMovieDB.count > 30)
+    alert('Вы киноман');
+else
+    alert('Произошла ошибка');
+
 console.log(personalMovieDB);
